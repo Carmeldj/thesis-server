cbe53e776d95e42126a93f7ee0b44eeb36d1d09f
Carmdl <carmel.djegui@epitech.eu>
2025-06-08 18:00:55 +0100

    update db url

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,11 +5,13 @@ const configService = new ConfigService();
 
 const AppDataSource = new DataSource({
   type: 'postgres',
-  host: configService.get<string>('DB_HOST'),
+  ssl: true,
+  url: 'postgresql://carmdj:gO1Z1ZwQa8AFKIMI4NTd3CdK2QUUChHP@dpg-d12rtkemcj7s73fn1mbg-a.frankfurt-postgres.render.com/liveshop',
+  // host: configService.get<string>('DB_HOST'),
   port: parseInt(configService.get<string>('DB_PORT')!, 5432),
-  username: 'carmdj',
-  password: '7879',
-  database: 'liveshop',
+  // username: 'carmdj',
+  // password: '7879',
+  // database: 'liveshop',
   synchronize: false,
   entities: ['**/*.entity.ts'],
   migrations: ['src/migrations/*-migration.ts'],
